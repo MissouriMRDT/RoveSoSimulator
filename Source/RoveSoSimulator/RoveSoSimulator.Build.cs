c3f9a184f436665e3220016354e24c1f770df216
clayjay3 <claytonraycowen@gmail.com>
2024-09-24 14:50:17 -0500

    Add ExternalLibrary plugin for RoveComm. Remove old RoveComm files.

--- Source/RoveSoSimulator/RoveSoSimulator.Build.cs
+++ Source/RoveSoSimulator/RoveSoSimulator.Build.cs
@@ -18,33 +18,5 @@ public class RoveSoSimulator : ModuleRules
             "Engine",
             "InputCore",
         });
-
-        // Load RoveComm based on the platform
-        LoadRoveComm(Target);
-    }
-
-    private void LoadRoveComm(ReadOnlyTargetRules Target)
-    {
-        // Define paths for both Windows and Linux libraries
-        string BasePath = Path.Combine(ModuleDirectory, "ThirdParty");
-
-        if (Target.Platform == UnrealTargetPlatform.Win64)
-        {
-            // Windows-specific paths
-            string IncludePath = Path.Combine(BasePath, "RoveComm_CPP_WIN64", "include");
-            string LibPath = Path.Combine(BasePath, "RoveComm_CPP_WIN64", "lib");
-
-            PublicIncludePaths.Add(IncludePath);
-            PublicAdditionalLibraries.Add(Path.Combine(LibPath, "libRoveComm_CPP.a"));
-        }
-        else if (Target.Platform == UnrealTargetPlatform.Linux)
-        {
-            // Linux-specific paths
-            string IncludePath = Path.Combine(BasePath, "RoveComm_CPP_AMD64", "include");
-            string LibPath = Path.Combine(BasePath, "RoveComm_CPP_AMD64", "lib");
-
-            PublicIncludePaths.Add(IncludePath);
-            PublicAdditionalLibraries.Add(Path.Combine(LibPath, "libRoveComm_CPP.a"));
-        }
     }
 }
