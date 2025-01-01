5e58d104e28bd8d2c62a8b6c465aca95469a7d0b
clayjay3 <claytonraycowen@gmail.com>
2025-01-01 17:16:59 -0600

    Working on AR Tag randomized placement.

--- Resources/start.bat
+++ Resources/start.bat
@@ -39,7 +39,7 @@ set GAME_URL=ws://127.0.0.1:80
 
 if exist "%GAME_EXECUTABLE%" (
     echo Starting game with URL %GAME_URL%...
-    start "Game" %GAME_EXECUTABLE% -PixelStreamingURL="%GAME_URL%" -PixelStreamingHudStats=true -PixelStreamingWebRTCFps=30 -PixelStreamingWebRTCStartBitrate=10000000 -PixelStreamingWebRTCDisableFrameDropper=true -PixelStreamingWebRTCVideoPacingMaxDelay=50 -PixelStreamingEncoderMaxQP=15
+    start "Game" %GAME_EXECUTABLE% -PixelStreamingURL="%GAME_URL%" -PixelStreamingHudStats=true -PixelStreamingHudStats=true -PixelStreamingWebRTCDisableFrameDropper=true -PixelStreamingWebRTCVideoPacingMaxDelay=50
 ) else (
     echo Game executable not found: %GAME_EXECUTABLE%
     exit /b 1
