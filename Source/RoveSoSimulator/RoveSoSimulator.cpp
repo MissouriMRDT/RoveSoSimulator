73777cf2e647f54405c09aae8fca62758c1eb761
clayjay3 <claytonraycowen@gmail.com>
2025-06-23 12:00:28 -0500

    Cleanup useless project files.

--- Source/RoveSoSimulator/RoveSoSimulator.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "RoveSoSimulator.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RoveSoSimulator, "RoveSoSimulator" );
